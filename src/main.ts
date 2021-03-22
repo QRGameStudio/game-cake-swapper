25859b1ff294741d6d5adb0abb771753da1f3759
StastnyJ <xstastnyj@gmail.com>
2021-03-22 09:44:33 +0100

    initial version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-const init = () => {
-    const content = document.getElementById("game-content");
-    content.innerHTML = "";
+/// <reference path="./game.ts" />
+
+const newGame = (height: number) => {
+  // @ts-ignore
+  new GTheme().apply();
+  const game = new Game(height, () => {
+    alert("Congratulations, you won");
+    newGame(4);
+  });
+
+  document.getElementById("leftBtn").onclick = () => game.click(0);
+  document.getElementById("middleBtn").onclick = () => game.click(1);
+  document.getElementById("rightBtn").onclick = () => game.click(2);
 };
 
-window.onload = init;
+newGame(4);
